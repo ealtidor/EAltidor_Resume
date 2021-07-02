cd800741ee639c3a8aa8b9e22dc28aebe03ad1a0
ealtidor <emanuella.altidor@gmail.com>
2021-07-01 22:10:22 -0400

    Created React App added nav componet

---
+++ altidor-portfolio/src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+
+export default function Nav() {
+  return (
+    <div>
+      <h1>Emanuella Altidor // Software Engineer </h1>
+      <h3>About</h3>
+      <h3>Recent Projects</h3>
+      <h3>Career</h3>
+      <h3>Resume</h3>
+      <h3>Contact</h3>
+    </div>
+  )
+}
